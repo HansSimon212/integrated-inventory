091dd6df52cff17a868b817280431d2dd5a3e983
Hans Simon <simon.h@husky.neu.edu>
2020-08-11 15:51:54 -0400

    Adds add/remove item functionality

--- src/App.js
+++ src/App.js
@@ -31,14 +31,67 @@ function createInitialItem(id) {
 
 function App() {
   const [items, setItems] = useState(createInitialInventory);
+  const textFieldRef = useRef();
+  const errorRef = useRef('');
+
+  function handleAddItem(e) {
+    let name = textFieldRef.current.value;
+        // disallows empty names
+    if(name === '') {return;}
+
+    for (const item of items) {
+      if (item.name === name) {
+        // TODO: display error message on addition of item with existing name
+        return;
+      }
+    }
+
+    let newItem = {
+      id: 2055,
+      name: name,
+      explosive: true
+    }
+    
+    setItems(prev => [...prev, newItem]);
+  }
+
+  // Event -> Void
+  // Clears the item entry text field
+  function handleClear(e) {
+    textFieldRef.current.value = null;
+  }
+
+  // Event -> Void
+  // Removes the item with the given name from the inventory
+  function handleRemoveItem(e) {
+    
+    let name = textFieldRef.current.value;
+    if(name === '') return;
+    
+    setItems(items => {
+      let newItems = items.splice(0,0);
+    
+      for(let i = 0; i < items.length; i++) {
+        if(items[i].name === name) {
+          newItems.splice(i,1);
+          return newItems;
+        }
+      }
+    });
+  }
 
   return (
     <>
       <Header />
 
   {/* Input Section*/}
+  <input type="text" ref={textFieldRef}/>
+  <button onClick={handleAddItem}>Add Item</button>
+  <button onClick={handleClear}>Clear</button>
+  <button onClick={handleRemoveItem}>Remove Item</button>
 
       <Inventory items={items} />
+
     </>
   );
 }
