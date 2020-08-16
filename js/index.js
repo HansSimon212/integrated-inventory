5d21f7148b6b0cec02b9c483729f890a27fce539
Hans <simon.h@husky.neu.edu>
2020-08-16 17:09:52 -0400

    Downloads html5qrcode npm package, continues work on scanner

--- js/index.js
+++ js/index.js
@@ -1,57 +1,5 @@
-/*
-    Global variables
-*/
-var cameraId = null;
-
-// This method will trigger user permissions
-Html5Qrcode.getCameras()
-  .then((devices) => {
-    /**
-     * devices would be an array of objects of type:
-     * { id: "id", label: "label" }
-     */
-    if (devices && devices.length) {
-      cameraId = devices[0].id;
-      // .. use this to start scanning.
-    }
-  })
-  .catch((err) => {
-    // handle err
-  });
-
-// // Starts using the camera
-// const html5QrCode = new Html5Qrcode(
-//   /* element id */ "reader",
-//   /* verbose */ true
-// );
-// html5QrCode
-//   .start(
-//     cameraId,
-//     {
-//       fps: 10, // Optional frame per seconds for qr code scanning
-//       qrbox: 150, // Optional if you want bounded box UI
-//     },
-//     (qrCodeMessage) => {
-//       html5QrCode
-//         .stop()
-//         .then((ignore) => {
-//           // QR Code scanning is stopped.
-//         })
-//         .catch((err) => {
-//           // Stop failed, handle it.
-//         });
-
-//       console.log(qrCodeMessage);
-//     },
-//     (errorMessage) => {
-//       console.log(`error: ${errorMessage}`);
-//     }
-//   )
-//   .catch((err) => {
-//     console.log(`Failed to start using camera id:${cameraId}`);
-//   });
-
 const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+
 // File based scanning
 const fileinput = document.getElementById("qr-input-file");
 fileinput.addEventListener("change", (e) => {
@@ -60,10 +8,10 @@ fileinput.addEventListener("change", (e) => {
     return;
   }
 
+  // Use the first item in the list
   const imageFile = e.target.files[0];
-  // Scan QR Code
   html5QrCode
-    .scanFile(imageFile, true)
+    .scanFile(imageFile, /* showImage= */ true)
     .then((qrCodeMessage) => {
       // success, use qrCodeMessage
       console.log(qrCodeMessage);
