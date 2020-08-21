a4d27ce2123d2040cc81e0c38b8dda3ab2cf040f
Hans <simon.h@husky.neu.edu>
2020-08-21 15:41:26 -0400

    Consolidates $_SESSION variables into arrays for each of {raw, dispersion, batch card)

--- assets/include/get-session-vars.php
+++ assets/include/get-session-vars.php
@@ -3,45 +3,25 @@
 session_start();
 
 /*
-* $_SESSION variables:
-* - status: one of {'scanning', 'info', 'success'}
-* - sender
-* - err_msg: error message passed to be displayed in browser
-* - item_uid
-* - item_rm
-* - item_lot
-* - item_name
-* - item_exp_date
-* - item_location
-* - item_quantity_kg
-* - item_container
-* - item_notes
+-----------------------------------------------
+            $_SESSION variables:
+-----------------------------------------------
+- status :String
+- sender :String
+- err_msg :String
+- item_uid :String
+- rm_info :Array
+- dispersion_info :Array
+- batch_card_info :Array 
 */
 
 $status = (isset($_SESSION['status']) ? $_SESSION['status'] : '');
 $sender = (isset($_SESSION['sender']) ? $_SESSION['sender'] : '');
 $err_msg = (isset($_SESSION['err_msg']) ? $_SESSION['err_msg'] : '');
 $item_uid = (isset($_SESSION['item_uid']) ? $_SESSION['item_uid'] : '');
-$item_rm = (isset($_SESSION['item_rm']) ? $_SESSION['item_rm'] : '');
-$item_lot = (isset($_SESSION['item_lot']) ? $_SESSION['item_lot'] : '');
-$item_name = (isset($_SESSION['item_name']) ? $_SESSION['item_name'] : '');
-$item_exp_date = (isset($_SESSION['item_exp_date']) ? $_SESSION['item_exp_date'] : '');
-$item_location = (isset($_SESSION['item_location']) ? $_SESSION['item_location'] : '');
-$item_quantity_kg = (isset($_SESSION['item_quantity_kg']) ? $_SESSION['item_quantity_kg'] : '');
-$item_container = (isset($_SESSION['item_container']) ? $_SESSION['item_container'] : '');
-$item_notes = (isset($_SESSION['item_notes']) ? $_SESSION['item_notes'] : '');
+$rm_info = (isset($_SESSION['rm_info']) ? $_SESSION['rm_info'] : array());
+$dispersion_info = (isset($_SESSION['dispersion_info']) ? $_SESSION['dispersion_info'] : array());
+$batch_card_info = (isset($_SESSION['batch_card_info']) ? $_SESSION['batch_card_info'] : array());
 
 // Unsets all items after they've been copied
 session_destroy();
-// unset($_SESSION['status']);
-// unset($_SESSION['sender']);
-// unset($_SESSION['err_msg']);
-// unset($_SESSION['item_uid']);
-// unset($_SESSION['item_rm']);
-// unset($_SESSION['item_lot']);
-// unset($_SESSION['item_name']);
-// unset($_SESSION['item_exp_date']);
-// unset($_SESSION['item_location']);
-// unset($_SESSION['item_quantity_kg']);
-// unset($_SESSION['item_container']);
-// unset($_SESSION['item_notes']);
--- database/get-info.php
+++ database/get-info.php
@@ -1,21 +1,78 @@
 <?php
 session_start();
 
-require("db_con.php");
-
 $item_uid = $_POST['item_uid']; // form: '{Number}' + {'B' | 'R' | 'D'}
 $sender = $_POST['sender']; // address this script was invoked from
+$destination = "Location: ../src/" . $sender;
 
-// Returns an error if item_uid is poorly formed
-if (strlen($item_uid) < 2) {
-    $_SESSION['err_msg'] = 'Poorly formed uid: ' . $item_uid;
-    // Closes database connection
-    $con->close();
-    // Returns to calling page
-    $destination = "Location: ../src/" . $sender;
+// resetSessionVars: Void -> Void
+// Resets all session variables and starts a new session (creates a clean slate)
+function resetSessionVars()
+{
+    session_destroy();
+    session_start();
+}
+
+// returnToSender: String Boolean -> Void
+// Sets error message session variable and returns to calling script. If
+// database connection is active/open, closes it.
+function returnToSender($errMsg, $resetSessionVars)
+{
+    // gets global reference
+    global $destination, $con;
+    // closes database conn. if conn. has been attempted and succeeded
+    isset($con) and $con and $con->close();
+    // clears all session variables if requested
+    $resetSessionVars and resetSessionVars();
+    // sets error message before return 
+    $_SESSION['err_msg'] = $errMsg;
+    // returns to calling script
     header($destination);
     exit();
 }
+
+// connectToDB(): String -> PDO
+// Creates a PDO (PHP Data Object) connection to database
+function connectToDB()
+{
+    // Variables required for MySQL connection
+    $server = "208.109.166.118";
+    $username = 'danielwilczak';
+    $password = 'Dmw0234567';
+    $database = "LEI_Inventory";
+
+    // Attempts connection to database
+    global $con;
+    $con = mysqli_connect($server, $username, $password, $database);
+
+    // Returns with an error message if error occurs
+    if (mysqli_connect_errno()) {
+        returnToSender($con->error, true);
+    }
+}
+
+// queryDatabase(): String -> Array
+// Queries the database with the given query, returns array representing result if query
+// was successful and return is non-empty
+function queryDatabase($sql)
+{
+    global $con, $item_uid;
+
+    $result = $con->query($sql);
+
+    if (!$result) {
+        // query failed
+        returnToSender('Database query failed: <br> uid:' . $item_uid . '<br>query: ' . $sql . '<br>Error: ' . $con->error, true);
+    }
+    if ($result->num_rows < 1) {
+        // return of query was empty
+        returnToSender('No matching entry in database found for uid: ' . $item_uid, true);
+    }
+    return mysqli_fetch_array($result);
+}
+
+// Returns an error if item_uid is poorly formed
+(strlen($item_uid) < 2) and returnToSender('Poorly formed uid: ' . $item_uid, true);
 
 // splits uid into number and letter representing item type
 $uid_num = substr($item_uid, 0, -1);
@@ -23,47 +80,24 @@ $casted_uid_num = intval($item_uid);
 $item_type = $item_uid[strlen($item_uid) - 1];
 
 // Returns an error if item_uid doesn't contain valid number
-if ($casted_uid_num <= 0) {
-    $_SESSION['err_msg'] = 'Poorly formed uid: ' . $item_uid;
-    // Closes database connection
-    $con->close();
-    // Returns to calling page
-    $destination = "Location: ../src/" . $sender;
-    header($destination);
-    exit();
-}
+($casted_uid_num <= 0) and returnToSender('Poorly formed uid: ' . $item_uid, true);
 
+// Attempts to connect to database
+connectToDB();
+
+// Builds query and queries the connected database
 $sql = "SELECT * FROM 29_RAW_INVENTORY WHERE uid=" . $casted_uid_num . "";
 
-$result = $con->query($sql);
-
-// Was database query completed successfully?
-if ($result == TRUE) {
-    // Was an entry with the given UID found?
-    if ($result->num_rows > 0) {
-        $item_info = mysqli_fetch_array($result);
-        $_SESSION['status'] = 'info';
-        $_SESSION['sender'] = $sender;
-        $_SESSION['err_msg'] = '';
-        $_SESSION['item_uid'] = $item_uid;
-        $_SESSION['item_rm'] = '';
-        $_SESSION['item_lot'] = '';
-        $_SESSION['item_name'] = $item_info['trade_Name'];
-        $_SESSION['item_exp_date'] = '';
-        $_SESSION['item_location'] = $item_info['location'];
-        $_SESSION['item_quantity_kg'] = '';
-        $_SESSION['item_container'] = '';
-        $_SESSION['item_notes'] = '';
-    } else {
-        // No entries found. Returns to scanning
-        $_SESSION['err_msg'] = 'No matching entry in database found for uid: ' . $item_uid;
-    }
-} // Query unsuccessful. Returns to scanning
-else {
-    $_SESSION['err_msg'] = 'Database query failed: <br> uid:' . $item_uid . '<br>query: ' . $sql . '<br>Error: ' . $con->error;
-}
-// Closes database connection
-$con->close();
-// Returns to calling page
-$destination = "Location: ../src/" . $sender;
-header($destination);
+$_SESSION['status'] = 'info';
+$_SESSION['sender'] = $sender;
+$_SESSION['err_msg'] = '';
+$_SESSION['item_uid'] = $item_uid;
+$_SESSION['item_rm'] = '';
+$_SESSION['item_lot'] = '';
+$_SESSION['item_name'] = $item_info['name'];
+$_SESSION['item_exp_date'] = '';
+$_SESSION['item_location'] = $item_info['location'];
+$_SESSION['item_quantity_kg'] = '';
+$_SESSION['item_container'] = '';
+$_SESSION['item_notes'] = '';
+returnToSender('', false);
