2248b277b3f04ab40dc357dd5667592ed9966201
Hans <simon.h@husky.neu.edu>
2020-08-21 12:29:00 -0400

    Ensures session variables aren't read when not set

--- assets/include/get-session-vars.php
+++ assets/include/get-session-vars.php
@@ -1,47 +1,47 @@
-<?php
-// Starts a session so the $_SESSION variables can be retrieved
-session_start();
-
-/*
- * $_SESSION variables:
- * - status: one of {'scanning', 'info', 'success'}
- * - sender
- * - err_msg: error message passed to be displayed in browser
- * - item_uid
- * - item_rm
- * - item_lot
- * - item_name
- * - item_exp_date
- * - item_location
- * - item_quantity_kg
- * - item_container
- * - item_notes
- */
-
-$status = $_SESSION['status'];
-$sender = $_SESSION['sender'];
-$err_msg = $_SESSION['err_msg'];
-$item_uid = $_SESSION['item_uid'];
-$item_rm = $_SESSION['item_rm'];
-$item_lot = $_SESSION['item_lot'];
-$item_name = $_SESSION['item_name'];
-$item_exp_date = $_SESSION['item_exp_date'];
-$item_location = $_SESSION['item_location'];
-$item_quantity_kg = $_SESSION['item_quantity_kg'];
-$item_container = $_SESSION['item_container'];
-$item_notes = $_SESSION['item_notes'];
-
-// Unsets all items after they've been copied
-unset($_SESSION['status']);
-unset($_SESSION['sender']);
-unset($_SESSION['err_msg']);
-unset($_SESSION['item_uid']);
-unset($_SESSION['item_rm']);
-unset($_SESSION['item_lot']);
-unset($_SESSION['item_name']);
-unset($_SESSION['item_exp_date']);
-unset($_SESSION['item_location']);
-unset($_SESSION['item_quantity_kg']);
-unset($_SESSION['item_container']);
-unset($_SESSION['item_notes']);
-?>
+<?php
+// Starts a session so the $_SESSION variables can be retrieved
+session_start();
+
+/*
+* $_SESSION variables:
+* - status: one of {'scanning', 'info', 'success'}
+* - sender
+* - err_msg: error message passed to be displayed in browser
+* - item_uid
+* - item_rm
+* - item_lot
+* - item_name
+* - item_exp_date
+* - item_location
+* - item_quantity_kg
+* - item_container
+* - item_notes
+*/
+
+$status = (isset($_SESSION['status']) ? $_SESSION['status'] : '');
+$sender = (isset($_SESSION['sender']) ? $_SESSION['sender'] : '');
+$err_msg = (isset($_SESSION['err_msg']) ? $_SESSION['err_msg'] : '');
+$item_uid = (isset($_SESSION['item_uid']) ? $_SESSION['item_uid'] : '');
+$item_rm = (isset($_SESSION['item_rm']) ? $_SESSION['item_rm'] : '');
+$item_lot = (isset($_SESSION['item_lot']) ? $_SESSION['item_lot'] : '');
+$item_name = (isset($_SESSION['item_name']) ? $_SESSION['item_name'] : '');
+$item_exp_date = (isset($_SESSION['item_exp_date']) ? $_SESSION['item_exp_date'] : '');
+$item_location = (isset($_SESSION['item_location']) ? $_SESSION['item_location'] : '');
+$item_quantity_kg = (isset($_SESSION['item_quantity_kg']) ? $_SESSION['item_quantity_kg'] : '');
+$item_container = (isset($_SESSION['item_container']) ? $_SESSION['item_container'] : '');
+$item_notes = (isset($_SESSION['item_notes']) ? $_SESSION['item_notes'] : '');
+
+// Unsets all items after they've been copied
+session_destroy();
+// unset($_SESSION['status']);
+// unset($_SESSION['sender']);
+// unset($_SESSION['err_msg']);
+// unset($_SESSION['item_uid']);
+// unset($_SESSION['item_rm']);
+// unset($_SESSION['item_lot']);
+// unset($_SESSION['item_name']);
+// unset($_SESSION['item_exp_date']);
+// unset($_SESSION['item_location']);
+// unset($_SESSION['item_quantity_kg']);
+// unset($_SESSION['item_container']);
+// unset($_SESSION['item_notes']);
--- src/index.php
+++ src/index.php
@@ -1,10 +1,16 @@
+<?php
+// Retrieves all $_SESSION vars (whether initialized or not)
+require("../assets/include/get-session-vars.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8"/>
+    <meta charset="UTF-8" />
     <title>Inventory Manager</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
     <?php
     require("../assets/include/import.html");
@@ -12,20 +18,20 @@
 </head>
 
 <body>
-<?php
-require("../assets/include/header.html");
-require("../assets/include/vrm.html");
-?>
+    <?php
+    require("../assets/include/header.html");
+    require("../assets/include/vrm.html");
+    ?>
 
-<div class="wrapper">
-    <section id="main_content">
-        <h2 class="main_content_title">Home</h2>
-        <hr>
-        <?php
-        require("../assets/include/error.php");
-        ?>
-    </section>
-</div>
+    <div class="wrapper">
+        <section id="main_content">
+            <h2 class="main_content_title">Home</h2>
+            <hr>
+            <?php
+            require("../assets/include/error.php");
+            ?>
+        </section>
+    </div>
 
 </body>
 
