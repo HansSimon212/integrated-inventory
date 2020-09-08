ec9bf9f473818f17fba75508cf4d7bec731f0255
Hans <simon.h@husky.neu.edu>
2020-09-08 17:41:55 -0400

    Edits index.php so it displays something

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo 'Hello World, this is MEEE I AM ALIVE'
+<?php echo 'Hello World, this is MEEE I AM ALIVE' ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>TEST</h1>
+</body>
+
+</html>
