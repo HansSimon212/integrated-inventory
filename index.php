0331949250c38c94596d14e32117e32eefab05e3
Hans <simon.h@husky.neu.edu>
2020-09-08 17:50:12 -0400

    Redirects index.php to scan.php (short term fix until project structure is fixed)

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
-<?php echo 'Hello World, this is MEEE I AM ALIVE' ?>
-
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>TEST</h1>
-</body>
-
-</html>
+<?php
+header("src/scan.php");
